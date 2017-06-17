50c217ff052a96a2012d0757a39181ac6baae8ce
Raffaello Bertini <raffaellobertini@gmail.com>
2017-06-17 01:42:36 +0100

    added flash test and page links in the / refactor flash template

--- server/test/ApplicationSpec.scala
+++ server/test/ApplicationSpec.scala
@@ -1,8 +1,6 @@
-import org.specs2.mutable._
 import org.specs2.runner._
 import org.junit.runner._
 import play.api.test._
-import play.api.test.Helpers._
 
 /**
   * Add your spec here.
@@ -10,7 +8,7 @@ import play.api.test.Helpers._
   * For more information, consult the wiki.
   */
 @RunWith(classOf[JUnitRunner])
-class ApplicationSpec extends Specification {
+class ApplicationSpec extends PlaySpecification {
   // Set sequential execution
   sequential
 
@@ -24,6 +22,20 @@ class ApplicationSpec extends Specification {
       status(home) must equalTo(OK)
       contentType(home) must beSome.which(_ == "text/html")
       contentAsString(home) must contain("Your new application is ready.")
+    }
+
+    "click on HangMan redirect to /newgame" in new WithBrowser {
+      browser.goTo("http://localhost:" + port)
+      browser.pageSource must contain("HangMan Game")
+      browser.click("a[id='hangman']")
+      browser.url() must contain("/new")
+    }
+
+    "click on HangMan redirect to /spa" in new WithBrowser {
+      browser.goTo("http://localhost:" + port)
+      browser.pageSource must contain("HangMan Game SPA")
+      browser.click("a[id='hangman-spa']")
+      browser.url() must contain("/hangperson/spa")
     }
   }
 }
